d1b39a97169662d9937ea44c709530c41cf85819
Naveen Kumar <naveengouda802@gmail.com>
2026-02-03 18:40:00 +0530

    Prime_Number.py
    Signed-off-by: Naveen Kumar  <naveengouda802@gmail.com>

---
+++ Prime_Number.py
@@ -0,0 +1,25 @@
+# Using Prime Number 
+# Method 1: Simple Loop
+
+n = int(input("Enter number: "))
+if n <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+
+# Using Method 2: Function
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))
